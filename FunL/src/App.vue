06636f148546a7b3fca860353117f489ef5b06fc
JP Moregain <j.p.moregain15@gmail.com>
2023-05-18 11:02:20 -0400

    Initial boilerplate setup

---
+++ FunL/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="nav">
+      <nav>
+        <RouterLink to="/" class="navtab">Home</RouterLink>
+        <RouterLink to="/about" class="navtab">About</RouterLink>
+      </nav>
+    </div>
+  <RouterView class="view"/>
+</template>
+
+<style scoped>
+
+nav {
+  display: flex;
+  justify-content: center;
+  height: 5vh;
+  background-color: black;
+}
+
+.navtab {
+  height: 100%;
+  background-color: white;
+  text-align: center;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+.view {
+  width: 100vw;
+  height: 95vh;
+}
+
+</style>
---
+++ FunL/src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button>Get Data</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    background-color: grey;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
