8581a8889c01ea55abf6f7c2a300242bc10166e1
JP Moregain <j.p.moregain15@gmail.com>
2023-05-24 09:56:16 -0400

    Create Title component, dynamically render titles that are fetched from API

---
+++ FunL/src/components/Title.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="title">
+    <img :src="props.title?.posterURLs[185]" alt="Movie/series poster" class="title-poster">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: {
+    type: Object
+  }
+})
+</script>
+
+<style scoped>
+
+.title-poster {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- FunL/src/views/HomeView.vue
+++ FunL/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { inject, ref } from 'vue'
 import axios from 'axios'
+import Title from '@/components/Title.vue'
+
 const apiKey = import.meta.env.VITE_API_KEY
 const host = import.meta.env.VITE_HOST
 
@@ -30,51 +32,84 @@ const formatServices = () => {
   return s.slice(0, -1)
 }
 
-const getData = async () => {
+// const getData = async () => {
+//   const formattedServices = formatServices()
+
+//   const getPage = async (nextCursor = null) => {
+//     const options = {
+//       method: 'GET',
+//       url: 'https://streaming-availability.p.rapidapi.com/v2/search/basic',
+//       params: {
+//         country: 'us',
+//         services: formattedServices,
+//         cursor: nextCursor
+//       },
+//       headers: {
+//         'X-RapidAPI-Key': apiKey,
+//         'X-RapidAPI-Host': host
+//       }
+//     }
+
+//     try {
+//       const response = await axios.request(options)
+//       const { data } = response
+//       searchResults.value = searchResults.value.concat(data.result)
+
+//       if (data.hasMore) {
+//         await getPage(data.nextCursor)
+//       } else {
+//         console.log(searchResults)
+//       }
+//     } catch (error) {
+//       console.error(error)
+//     }
+//   }
+
+//   await getPage()
+// }
+
+const getDataTest = async () => {
   const formattedServices = formatServices()
 
-  const getPage = async (nextCursor = null) => {
-    const options = {
-      method: 'GET',
-      url: 'https://streaming-availability.p.rapidapi.com/v2/search/basic',
-      params: {
-        country: 'us',
-        services: formattedServices,
-        cursor: nextCursor
-      },
-      headers: {
-        'X-RapidAPI-Key': apiKey,
-        'X-RapidAPI-Host': host
-      }
-    }
-
-    try {
-      const response = await axios.request(options)
-      const { data } = response
-      searchResults.value = searchResults.value.concat(data.result)
-
-      if (data.hasMore) {
-        await getPage(data.nextCursor)
-      } else {
-        console.log(searchResults)
-      }
-    } catch (error) {
-      console.error(error)
+  const options = {
+    method: 'GET',
+    url: 'https://streaming-availability.p.rapidapi.com/v2/search/basic',
+    params: {
+      country: 'us',
+      services: formattedServices
+    },
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': host
     }
   }
 
-  await getPage()
+  try {
+    const response = await axios.request(options)
+    const { data } = response
+    searchResults.value = searchResults.value.concat(data.result)
+    console.log(searchResults)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 const logResults = () => {
+  console.log(searchResults)
+  searchResults.value.splice(4)
   console.log(searchResults)
 }
 </script>
 
 <template>
   <div class="home">
-    <button @click="getData">Get Data</button>
-    <button @click="logResults">Get Data</button>
+    <button @click="getDataTest">Get Data</button>
+    <button @click="logResults">LogResults</button>
+    <div id="title-container">
+      <div v-for="title in searchResults" :key="title">
+        <Title :title="title" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -85,7 +120,14 @@ const logResults = () => {
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  #title-container {
+    display: grid;
+    width: 100%;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
     justify-content: center;
+    align-items: center;
   }
 }
 </style>
