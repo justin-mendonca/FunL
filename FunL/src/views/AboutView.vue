c5cc366f535bed41e13dc08d5a2722d447e05116
JP Moregain <j.p.moregain15@gmail.com>
2023-08-30 07:48:15 -0400

    Delete unused images, delete placeholder code from AboutView module, add accordion component

--- FunL/src/views/AboutView.vue
+++ FunL/src/views/AboutView.vue
@@ -1,31 +1,40 @@
 <script setup lang="ts">
-import axios from 'axios'
-const apiKey = import.meta.env.VITE_API_KEY;
-const host = import.meta.env.VITE_HOST;
-
-const incrementCounter = async () => {
-  const options = {
-    method: 'GET',
-    url: 'https://streaming-availability.p.rapidapi.com/v2/services',
-    headers: {
-      'X-RapidAPI-Key': apiKey,
-      'X-RapidAPI-Host': host
-    }
-  }
-
-  try {
-    const response = await axios.request(options)
-    console.log(response.data)
-  } catch (error) {
-    console.error(error)
-  }
-}
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
 </script>
 
 <template>
   <div class="about">
-    <h1>Click the button below to get data about the available streaming services</h1>
-    <button @click="incrementCounter">Click me</button>
+    <Accordion :multiple="true" :activeIndex="[0]" class="accordion">
+      <AccordionTab header="Header I">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </AccordionTab>
+      <AccordionTab header="Header II">
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+          architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+          voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+        </p>
+      </AccordionTab>
+      <AccordionTab header="Header III">
+        <p>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+          voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+          cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
+          est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
+          libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+        </p>
+      </AccordionTab>
+    </Accordion>
   </div>
 </template>
 
@@ -36,6 +45,10 @@ const incrementCounter = async () => {
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  }
+
+  .accordion {
+    width: 50%;
   }
 }
 </style>
